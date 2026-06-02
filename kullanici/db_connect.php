7f6e9aef60c1e073a902b1a2c9980e11ac191d70
Nisa Nur Doğan <nisanrdgn1006@gmail.com>
2026-06-02 19:04:48 +0300

    Add files via upload

---
+++ kullanici/db_connect.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$dbname = "isletme_rehberi";
+$username = "root";
+$password = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Veritabanı bağlantı hatası: " . $e->getMessage());
+}
+
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
